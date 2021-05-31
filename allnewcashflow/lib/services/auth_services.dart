cd7dd0f7aea914b4a612a152a395df2cfa16d709
Fadheljanuar <62705800+Fadheljanuar@users.noreply.github.com>
2021-05-31 20:33:25 +0700

    Revert "8"
    This reverts commit 06151ca541339340a61b5cf9920788ebb3af0a69.

--- allnewcashflow/lib/services/auth_services.dart
+++ allnewcashflow/lib/services/auth_services.dart
@@ -6,17 +6,9 @@ class AuthServices {
       FirebaseFirestore.instance.collection("users");
   static DocumentReference userDoc;
 
-  static Future<String> signUp(Users users) async {
+  static Future<String> signUp() async {
     await Firebase.initializeApp();
-    String dateNow = ActivityServices.dateNow();
     String msg = "";
-    String token = "";
-    String uid = "";
-
-    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
-        email: users.email, password: users.password);
-    uid = userCredential.user.uid;
-    token = await userCredential.user.getIdToken();
 
     await userCollection.doc(uid).set({
       'uid': uid,
