82be241e5dc218176c83b02882239be627f5950c
Fadheljanuar <62705800+Fadheljanuar@users.noreply.github.com>
2021-06-03 13:29:59 +0700

    test again

--- allnewcashflow/lib/services/auth_services.dart
+++ allnewcashflow/lib/services/auth_services.dart
@@ -8,17 +8,9 @@ class AuthServices {
       FirebaseFirestore.instance.collection("users");
   static DocumentReference userDoc;
 
-  static Future<String> signUp(Users users) async {
+  static Future<String> signUp() async {
     await Firebase.initializeApp();
-    String dateNow = ActivityServices.dateNow();
     String msg = "";
-    String token = "";
-    String uid = "";
-
-    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
-        email: users.email, password: users.password);
-    uid = userCredential.user.uid;
-    token = await userCredential.user.getIdToken();
 
     return msg;
   }
---
+++ allnewcashflow/lib/services/services.dart
@@ -0,0 +1,4 @@
+<<<<<<< HEAD
+=======
+part 'auth_services.dart';
+>>>>>>> parent of 06151ca (8)
